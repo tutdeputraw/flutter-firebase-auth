7505a5ce38c6249403ce617d1b5e6b34d739f8a3
tutdeputraw <anoboy.code@gmail.com>
2021-10-12 10:29:14 +0700

    init commit

---
+++ lib/pages/homepage/homepage.dart
@@ -0,0 +1,13 @@
+part of '../../pages/pages.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text('hei'),
+    );
+  }
+}
